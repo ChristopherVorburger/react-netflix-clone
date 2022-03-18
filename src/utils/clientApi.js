03a1553afccc101ae3941679b5af08a18134af17
Chris <christopher.vorburger2@gmail.com>
2022-03-18 14:43:44 -0600

    final exercice

--- src/utils/clientApi.js
+++ src/utils/clientApi.js
@@ -1,10 +1,13 @@
 import axios from 'axios'
 import {apiKey, lang, API_URL} from '../config'
 
-const clientApi = endpoint => {
+const sleep = t => new Promise(resolve => setTimeout(resolve, t))
+
+const clientApi = async endpoint => {
   const page = 1
   const startChar = endpoint.includes('?') ? `&` : `?`
-  const keyLang = `${startChar}api_key=${apiKey}&language=${lang}&page=${page}`
+  await sleep(2000)
+  const keyLang = `${startChar}api_skey=${apiKey}&language=${lang}&page=${page}`
   return axios.get(`${API_URL}/${endpoint}${keyLang}`)
 }
 
