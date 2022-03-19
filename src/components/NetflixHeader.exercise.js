8e505547d7a53c44f5be77adae7eb9fc24c19a43
Chris <christopher.vorburger2@gmail.com>
2022-03-19 14:54:55 -0600

    add function to add and delete bookmark and snackbar

--- src/components/NetflixHeader.exercise.js
+++ src/components/NetflixHeader.exercise.js
@@ -1,25 +1,22 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import {useFetchData} from 'utils/hooks'
 import {imagePathOriginal, TYPE_MOVIE} from '../config'
 import {HeaderSkeleton} from './skeletons/HeaderSkeleton'
+import {clientNetFlix} from '../utils/clientApi'
+import * as authNetflix from '../utils/authNetflixProvider'
+import DeleteIcon from '@mui/icons-material/Delete'
+import Snackbar from '@mui/material/Snackbar'
+import MuiAlert from '@mui/material/Alert'
 
-// 🐶 importe le hook 'useFetchData' depuis 'utils/hooks'
-// 🐶 importe le 'clientNetFlix' depuis 'utils/clientApi'
-// il nous permettra de faire les appels API REST avec 'axios'
-// 🐶 importe 'authNetflix' il nous sera utile pour récuperer 
-// le 'token' de l'utilisateur connecté
-// 🤖 import * as authNetflix from '../utils/authNetflixProvider'
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />
+})
 
-// 👨‍✈️ Trois choses à gérer dans ce composant :
-//
-// 1. Etat initial
-//  - vérifier si ce film/serie est déjà dans les favoris
-//  - faire un appel API /bookmark
-// 2. Ajouter aux favoris
-//  - faire un appel API POST /bookmark/tv ou /bookmark/movie
-// 3. Supprimer des favoris
-//  - faire un appel API DELETE /bookmark/tv ou /bookmark/movie
 const NetflixHeader = ({movie, type = TYPE_MOVIE}) => {
-  // 🐶 utilise le hook 'useFetchData' pour avoir : data, execute
+  const {data, status, error, execute} = useFetchData()
+  const [snackbarOpen, setSnackbarOpen] = React.useState(false)
+  const [callBookmark, setCallBookmark] = React.useState(false)
+
   const title = type === TYPE_MOVIE ? movie?.title : movie?.name
   const imageUrl = `${imagePathOriginal}${movie?.backdrop_path}`
   const banner = {
@@ -31,28 +28,50 @@ const NetflixHeader = ({movie, type = TYPE_MOVIE}) => {
     height: '448px',
   }
 
-   // 🐶 utilise le hook 'useEffect' pour faire l'appel API GET '/bookmark'
-   // pour cela utilise :
-   // 🤖
-   // authNetflix.getToken()
-   // execute(clientNetFlix(`bookmark`, {token}))
-   // NOTE : authNetflix.getToken() s'utilise de manière asynchrone avec 'await'
+  useEffect(() => {
+    // Get token
+    const getToken = async () => {
+      const token = await authNetflix.getToken()
+      execute(clientNetFlix(`bookmark`, {token}))
+    }
+    getToken()
+  }, [execute])
+  console.log('data', data)
 
-   // 🐶 créé un boolean 'isInList' permetant de s'avoir si 'movie.id' est deja 
-   // dans la liste des favoris récuperer par l'api '/bookmark'
-   // rapel du format des données reçues
-   // data.bookmark.movies[ids de films]
-   // data.bookmark.tv[ids de séries]
+  useEffect(() => {
+    setSnackbarOpen(true)
+  }, [status])
 
-   // 🐶 créé une fonction async 'handleAddToListClick' qui fera l'appel API REST
-   // '/bookmark/tv' ou '/bookmark/movie'
-   // utilise 'clientNetFlix' car il permet de passer des options : {token,data,method}
-   // - passe le 'token'
-   // - passe comme 'data' : id (l'id du film/serie)
-   // - passe come 'method' 'POST'
+  // Handle if the movie is already in the list
+  const isInList = data?.bookmark[
+    type === TYPE_MOVIE ? 'movies' : 'series'
+  ]?.includes(movie?.id)
 
-   // 🐶 créé une fonction async 'handleDeleteToListClick'
-   // pareil que précedement mais en utilisant la methode 'DELETE'
+  // Function to add a bookmark
+  const handleAddToListClick = async () => {
+    setCallBookmark(true)
+    const token = await authNetflix.getToken()
+    execute(
+      clientNetFlix(`bookmark/${type}`, {
+        token,
+        data: {id: movie.id},
+        method: 'POST',
+      }),
+    )
+  }
+
+  // Function to delete a bookmark
+  const handleDeleteToListClick = async () => {
+    setCallBookmark(true)
+    const token = await authNetflix.getToken()
+    execute(
+      clientNetFlix(`bookmark/${type}`, {
+        token,
+        data: {id: movie.id},
+        method: 'DELETE',
+      }),
+    )
+  }
 
   if (!movie) {
     return <HeaderSkeleton></HeaderSkeleton>
@@ -63,17 +82,52 @@ const NetflixHeader = ({movie, type = TYPE_MOVIE}) => {
         <h1 className="banner__title">{title ?? '...'}</h1>
         <div className="banner__buttons">
           <button className="banner__button banner__buttonplay">Lecture</button>
-          {/* 🐶 utilise 'isInList' pour afficher deux bouttons differents
-            1. Premier bouton : 'Supprimer de ma liste' avec onClick={handleDeleteToListClick}
-            2. Deuximème bouton : 'Ajouter à ma liste' onClick={handleAddToListClick}
-           */}
-          <button className="banner__button banner__buttonInfo">
-            Ajouter à ma liste
-          </button>
+          {isInList ? (
+            <button
+              className="banner__button banner__buttonInfo"
+              onClick={handleDeleteToListClick}
+            >
+              <DeleteIcon
+                color="secondary"
+                style={{marginRight: '5px'}}
+                fontSize={'small'}
+              />
+              Supprimer de ma liste
+            </button>
+          ) : (
+            <button
+              className="banner__button banner__buttonInfo"
+              onClick={handleAddToListClick}
+            >
+              Ajouter à ma liste
+            </button>
+          )}
         </div>
         <h1 className="synopsis">{movie?.overview ?? '...'}</h1>
       </div>
       <div className="banner--fadeBottom"></div>
+      {callBookmark && status === 'done' ? (
+        <Snackbar
+          open={snackbarOpen}
+          autoHideDuration={4000}
+          onClose={() => setSnackbarOpen(false)}
+        >
+          <Alert severity="success" sx={{width: '100%'}}>
+            Liste modifiée avec succès
+          </Alert>
+        </Snackbar>
+      ) : null}
+      {callBookmark && status === 'error' ? (
+        <Snackbar
+          open={snackbarOpen}
+          autoHideDuration={4000}
+          onClose={() => setSnackbarOpen(false)}
+        >
+          <Alert severity="error" sx={{width: '100%'}}>
+            Problème lors de l'ajout : {error.message}
+          </Alert>
+        </Snackbar>
+      ) : null}
     </header>
   )
 }
